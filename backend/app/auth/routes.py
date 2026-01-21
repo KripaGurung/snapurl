bff44b6a03c083fa6b2b540eee992e973e27cc39
KripaGurung <krepagurung@gmail.com>
2026-01-21 11:21:18 +0545

    add email-validator for pydantic EmailStr

--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -5,6 +5,7 @@ from ..db import SessionLocal
 from ..models import User
 from .utils import hash_password, verify_password
 from .jwt import create_access_token
+from .schemas import SignupRequest, LoginRequest
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
@@ -16,14 +17,14 @@ def get_db():
         db.close()
 
 @router.post("/signup")
-def signup(email: str, password: str, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == email).first()
+def signup(data: SignupRequest, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == data.email).first()
     if user:
         raise HTTPException(status_code=400, detail="User already exists")
 
     new_user = User(
-        email=email,
-        hashed_password=hash_password(password)
+        email=data.email,
+        hashed_password=hash_password(data.password)
     )
     db.add(new_user)
     db.commit()
@@ -32,9 +33,9 @@ def signup(email: str, password: str, db: Session = Depends(get_db)):
     return {"message": "User created successfully"}
 
 @router.post("/login")
-def login(email: str, password: str, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == email).first()
-    if not user or not verify_password(password, user.hashed_password):
+def login(data: LoginRequest, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == data.email).first()
+    if not user or not verify_password(data.password, user.hashed_password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
     token = create_access_token({"sub": user.email})
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .db import engine
 from . import models
 from .auth.routes import router as auth_router
@@ -7,6 +8,14 @@ from .shortener.routes import router as shortener_router
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="SnapUrl API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(shortener_router)
