809776677780c711418032bd8fabfea78d05a959
KripaGurung <krepagurung@gmail.com>
2026-02-01 13:23:57 +0545

    Remove duplicate BrowserRouter from App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,27 @@
 import './App.css'
-import {  BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Login from './pages/login.jsx';
 import Signup from './pages/signup.jsx';
 import CreateUrl from './pages/createUrl.jsx';
 
-function AppContent() {
-
-
-  return (
-    <>
-    <Routes>
-      <Route path="/" element={ <> <Navbar /> <CreateUrl />  </> } />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
-    </>
-  )
-}
-
 function App() {
   return (
-    <Router>
-      <AppContent />
-    </Router>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <CreateUrl />
+            </>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
   );
 }
 
